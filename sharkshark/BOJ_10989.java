94553f87f017a82acbb04893d694117b7bec7536
Jungyoon <37810852+JYKkkk@users.noreply.github.com>
2023-01-02 23:08:26 +0900

    시간 초과

---
+++ sharkshark/BOJ_10989.java
@@ -0,0 +1,27 @@
+package algori;
+import java.util.*;
+
+//import jdk.internal.org.jline.utils.InputStreamReader;
+
+import java.io.*;
+
+public class BOJ_10989 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		for(int n=0; n<N; n++) {
+			arr[n] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i=0; i<N; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
